58f153282d110283d665dfbcf724ea22560adae0
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-06-29 15:40:07 -0400

    Prepare for deploy using pym

---
+++ public/pymSetup.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+  const pymParent = new pym.Parent(
+    '2020-cost-recovery-map',
+    'https://cost-recovery-map-2020.netlify.app',
+    {}
+  );
+});
